1ebab32afdd6f1f0019e6c93dedf8c117c1cf71c
AndyWang25440568 <cmu.swang@gmail.com>
2012-10-14 11:52:37 -0400

    Some tests Added

--- spec/views/static_pages_spec.rb
+++ spec/views/static_pages_spec.rb
@@ -14,12 +14,12 @@ describe "Static Pages" do
   describe "Help Page" do
     it "should have the h1 'Help'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit help_path
+      visit '/static_pages/help'
       page.should have_selector('h1',:text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
+      visit '/static_pages/help'
       page.should have_selector('title',
                                 :text => "This is the help page for MyWebsites | Help")
     end
@@ -29,15 +29,15 @@ describe "Static Pages" do
   describe "About Us" do
     it "should have the content 'About Us'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit about_path
-      page.should have_content('About Us')
+      visit '/static_pages/about'
+      page.should have_content('About us')
     end
   end
 
   describe "Contact Page" do
     it "should have the h2 'Contact Us'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit contact_path
+      visit '/static_pages/contact'
       page.should have_selector('h2',:text => 'Contact Us')
     end
 
